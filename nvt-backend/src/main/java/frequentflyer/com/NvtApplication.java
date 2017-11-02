f56c023742cf5a3be8db643d2b2bd10090f3d2e3
Sasa Radovanovic <sasa.radovanovic@klm.com>
2017-11-02 16:19:26 +0100

    Initial data setup

--- nvt-backend/src/main/java/frequentflyer/com/NvtApplication.java
+++ nvt-backend/src/main/java/frequentflyer/com/NvtApplication.java
@@ -1,8 +1,5 @@
 package frequentflyer.com;
 
-import frequentflyer.com.domain.DomainMapper;
-import frequentflyer.com.domain.RotationDto;
-import frequentflyer.com.entities.AirlineRoute;
 import frequentflyer.com.entities.Airport;
 import frequentflyer.com.entities.Combination;
 import frequentflyer.com.entities.Rotation;
@@ -10,7 +7,7 @@ import frequentflyer.com.repositories.*;
 import frequentflyer.com.services.AirlineRouteService;
 import frequentflyer.com.services.AirlineService;
 import frequentflyer.com.services.AirportService;
-import frequentflyer.com.services.impl.AirlineServiceImpl;
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -22,14 +19,6 @@ import org.springframework.core.task.TaskExecutor;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.io.InputStream;
-
-import java.text.DateFormat;
-import java.time.Instant;
-import java.time.LocalTime;
-import java.time.ZoneId;
-import java.time.ZoneOffset;
-import java.util.Date;
-import java.util.Locale;
 
 
 @SpringBootApplication
@@ -125,11 +114,11 @@ public class NvtApplication implements CommandLineRunner {
 			airlineRouteService.loadAirlineRoutes(airlineRoutesInputStream);
 		}
 
-		Combination combination = combinationRepository.findByCombinationName("MyAirline");
+		Combination combination = combinationRepository.findByCombinationName("SuperAirline S2018");
 		if (combination == null) {
 
 			combination = new Combination();
-			combination.setCombinationName("MyAirline");
+			combination.setCombinationName("SuperAirline S2018");
 			combination.setCombinationColor("#6699ff");
 			combination = combinationRepository.save(combination);
 
@@ -139,100 +128,56 @@ public class NvtApplication implements CommandLineRunner {
 			Airport JFK = airportRepository.findByIataCode("JFK");
 			Airport GIG = airportRepository.findByIataCode("GIG");
 			Airport BEG = airportRepository.findByIataCode("BEG");
-			Airport LUX = airportRepository.findByIataCode("LUX");
+			Airport KVO = airportRepository.findByIataCode("KVO");
+			Airport ALA = airportRepository.findByIataCode("ALA");
+			Airport ZNZ = airportRepository.findByIataCode("ZNZ");
+			Airport ZAG = airportRepository.findByIataCode("ZAG");
+			Airport PUY = airportRepository.findByIataCode("PUY");
 
-			Rotation rotation1 = new Rotation();
-			rotation1.setCombination(combination);
-			rotation1.setOrigin(AMS);
-			rotation1.setDestination(CDG);
-			rotation1.setFlightLength(120);
-			rotation1.setFrequency("1/2/3/4/5/6/7");
-			rotation1.setLocalDepartureTime("10:00");
-			rotation1 = rotationRepository.save(rotation1);
+			saveRotation(combination, AMS, CDG, 90, "1/2/3/4/5/6/7", "07:00");
+			saveRotation(combination, CDG, AMS, 90, "1/2/3/4/5/6/7", "09:30");
 
-			Rotation rotation2 = new Rotation();
-			rotation2.setCombination(combination);
-			rotation2.setOrigin(AMS);
-			rotation2.setDestination(NRT);
-			rotation2.setFlightLength(600);
-			rotation2.setFrequency("1/4/5/6");
-			rotation2.setLocalDepartureTime("01:30");
-			rotation2 = rotationRepository.save(rotation2);
+			saveRotation(combination, AMS, NRT, 600, "1/2/3/4/5/6/7", "11:15");
+			saveRotation(combination, NRT, AMS, 620, "1/2/3/4/5/6/7", "18:45");
 
+			saveRotation(combination, AMS, BEG, 120, "1/2/3/4/5/6/7", "02:15");
+			saveRotation(combination, BEG, ZAG, 45, "1/2/3/4/5/6/7", "05:15");
+			saveRotation(combination, ZAG, ZAG, 45, "1/2/3/4/5/6/7", "07:15");
+			saveRotation(combination, BEG, AMS, 120, "1/2/3/4/5/6/7", "09:20");
 
-			Rotation rotation3 = new Rotation();
-			rotation3.setCombination(combination);
-			rotation3.setOrigin(AMS);
-			rotation3.setDestination(JFK);
-			rotation3.setFlightLength(420);
-			rotation3.setFrequency("1/2/3/4/5/6/7");
-			rotation3.setLocalDepartureTime("16:15");
-			rotation3 = rotationRepository.save(rotation3);
+			saveRotation(combination, AMS, BEG, 120, "1/2/3/4/5/6/7", "02:35");
+			saveRotation(combination, BEG, KVO, 30, "1/2/3/4/5/6/7", "05:15");
+			saveRotation(combination, KVO, BEG, 30, "1/2/3/4/5/6/7", "06:15");
+			saveRotation(combination, BEG, AMS, 120, "1/2/3/4/5/6/7", "08:45");
 
+			saveRotation(combination, AMS, PUY, 110, "1/3/5/7", "20:15");
+			saveRotation(combination, PUY, AMS, 110, "1/3/5/7", "23:35");
 
-			Rotation rotation4 = new Rotation();
-			rotation4.setCombination(combination);
-			rotation4.setOrigin(AMS);
-			rotation4.setDestination(BEG);
-			rotation4.setFlightLength(120);
-			rotation4.setFrequency("1/2/3/4/5/6/7");
-			rotation4.setLocalDepartureTime("02:15");
-			rotation4 = rotationRepository.save(rotation4);
+			saveRotation(combination, AMS, ALA, 365, "1/2/4/6", "15:00");
+			saveRotation(combination, ALA, AMS, 375, "1/2/4/6", "20:00");
 
+			saveRotation(combination, AMS, JFK, 480, "1/2/3/4/5/6/7", "16:15");
+			saveRotation(combination, JFK, AMS, 460, "1/2/3/4/5/6/7", "22:00");
 
-			Rotation rotation5 = new Rotation();
-			rotation5.setCombination(combination);
-			rotation5.setOrigin(AMS);
-			rotation5.setDestination(LUX);
-			rotation5.setFlightLength(55);
-			rotation5.setFrequency("1/2/3/4/5/6/7");
-			rotation5.setLocalDepartureTime("02:25");
-			rotation5 = rotationRepository.save(rotation5);
+			saveRotation(combination, AMS, GIG, 660, "1/2/3/4/5/6/7", "11:25");
+			saveRotation(combination, GIG, AMS, 680, "1/2/3/4/5/6/7", "16:00");
+
+			saveRotation(combination, AMS, ZNZ, 415, "1/2/3/4/5/6/7", "11:05");
+			saveRotation(combination, ZNZ, AMS, 425, "1/2/3/4/5/6/7", "19:25");
+
 		}
 
-
-		Airport schiphol = airportRepository.findByIataCode("AMS");
-		Airport jfk = airportRepository.findByIataCode("JFK");
-		Airport narita = airportRepository.findByIataCode("NRT");
-
-		Rotation example = rotationRepository.findByOriginAndDestinationAndCombination(schiphol, jfk, combination);
-
-		Instant instant = Instant.now(); //can be LocalDateTime
-		ZoneId schipholZone = schiphol.getTimezone().toZoneId(); // my timezone
-		ZoneOffset depZoneOffset = schipholZone.getRules().getOffset(instant);
-
-		ZoneId jfkZone = jfk.getTimezone().toZoneId(); // my timezone
-		ZoneOffset arrZoneOffset = jfkZone.getRules().getOffset(instant);
-
-		log.info(" AMS - JFK rotation ");
-		LocalTime departureTime = LocalTime.parse(example.getLocalDepartureTime());
-		departureTime.atOffset(depZoneOffset);
-
-		Locale locale = Locale.getDefault();
+	}
 
 
-		DateFormat formatter = DateFormat.getDateTimeInstance(
-				DateFormat.DEFAULT,
-				DateFormat.DEFAULT,
-				locale);
-
-		log.info(" AMS " + departureTime + " TIMEZONE OFFSET FROM UTC: " + schiphol.getTimezone().getOffset(new Date().getTime()));
-
-
-
-
-		LocalTime arrivalTime = departureTime.plusMinutes(example.getFlightLength());
-		arrivalTime.atOffset(arrZoneOffset);
-		log.info(" JFK " + arrivalTime + " TIMEZONE OFFSET FROM UTC: " + jfk.getTimezone().getOffset(new Date().getTime()));
-
-		Rotation rot = rotationRepository.findByOriginAndDestinationAndCombination(schiphol, narita, combination);
-
-		RotationDto rto1 = DomainMapper.rotationToRotationDto(example);
-		RotationDto rto2 = DomainMapper.rotationToRotationDto(rot);
-
-		log.info("AMS - NRT route {}", rto2.getDayMap());
-		log.info("AMS - NRT time {}", rto2.getUtcDepartureTime());
-		log.info("Flight time {}", rto2.getFlightTime());
-
+	private void saveRotation (Combination combination, Airport origin, Airport destination, int length, String frequency, String localDepartureTime) {
+		Rotation rotation = new Rotation();
+		rotation.setCombination(combination);
+		rotation.setOrigin(origin);
+		rotation.setDestination(destination);
+		rotation.setFlightLength(length);
+		rotation.setFrequency(frequency);
+		rotation.setLocalDepartureTime(localDepartureTime);
+		rotationRepository.save(rotation);
 	}
 }
